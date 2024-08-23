8f1aab39b2a11cab12048d2155c388d23a93757c
nanashi <egorglazkov71@gmail.com>
2024-08-23 13:25:49 +0300

    add src for hash table

---
+++ hash_table/src/HashTable.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+
+class HashTable:
+    def __init__(self):
+        self.__count = 0
+        self.__size = 2500
+        self.__array = [None] * self.__size
+
+    def __hash(self, key: any) -> int:
+        value = hash(key)
+        return value % self.__size
+
+    def add(self, item: any, key: any) -> None:
+        if item is None:
+            raise TypeError("Value of type None cannot be put in table ")
+
+        _hash = self.__hash(key)
+        if self.__array[_hash] is not None:
+            print("Collision")
+            return
+
+        self.__array[_hash] = item
+        self.__count += 1
+
+    def get(self, key: any) -> any:
+        _hash = self.__hash(key)
+
+        item = self.__array[_hash]
+        if item is None:
+            raise KeyError("Key not found")
+
+        return item
+
+    def delete(self, key: any) -> None:
+        _hash = self.__hash(key)
+
+        if self.__array[_hash] is None:
+            raise KeyError("Key not found")
+
+        self.__array[_hash] = None
+        self.__count -= 1
+
+    def clear(self) -> None:
+        self.__array = []
